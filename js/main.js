3ac3a8af8fd5a595631a0e9674614346f8dc3046
Jorge Fabrizio Lopez Quiroz <giorgiofabrilopez@gmail.com>
2021-03-29 17:32:05 +0200

    Esercizio completato
    - sto pensando al bonus

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        emails: [],
+    },
+
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((result) => {
+                console.log(result.data.response);
+                this.emails.push(result.data.response);
+         });
+        }
+    }
+})
+
+Vue.config.devtools = true
